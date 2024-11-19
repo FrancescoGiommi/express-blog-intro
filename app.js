3f0e1a294c16b3e4696627a7a9af5e9d57bd6f25
FrancescoGiommi <francescogiommi97@icloud.com>
2024-11-19 15:10:56 +0100

    Add post array and bacheca route

--- app.js
+++ app.js
@@ -16,12 +16,53 @@ visualizzare le immagini associate ad ogni post.
 
 Testare le chiamate su Postman! */
 
+/* Import express */
 const express = require("express");
 const app = express();
 const port = 3000;
+
+/* First route */
 app.get("/", (req, res) => {
   res.send("Server del mio blog");
 });
+
+const bachecaPost = [
+  {
+    titolo: "Ciambellone",
+    contenuto: "Ricetta ciambellone",
+    immagine: "images/ciambellone.jpeg",
+    tags: ["ciambellone", "ricetta", "dolce"],
+  },
+  {
+    titolo: "Cracker alla barbabietola",
+    contenuto: "Ricetta cracker alla barbabietola",
+    immagine: "images/cracker_barbabietola.jpeg",
+    tags: ["cracker", "spuntino", "dolce"],
+  },
+  {
+    titolo: "Pane fritto dolce",
+    contenuto: "Ricetta pane fritto dolce",
+    immagine: "images/pane_fritto_dolce.jpeg",
+    tags: ["pane fritto dolce", "ricetta", "dolce"],
+  },
+  {
+    titolo: "Pasta alla barbabietola",
+    contenuto: "Ricetta pasta alla barbabietola",
+    immagine: "images/pasta_barbabietola.jpeg",
+    tags: ["pasta alla barbabietola", "ricetta", "pasta"],
+  },
+  {
+    titolo: "Torta paesana",
+    contenuto: "Ricetta Torta paesana",
+    immagine: "images/torta_paesana.jpeg",
+    tags: ["torta paesana", "ricetta", "dolce"],
+  },
+];
+
+app.get("/bacheca", (req, res) => {
+  res.send(bachecaPost);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port`);
 });
